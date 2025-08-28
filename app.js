e7a84fd362b1e60044f57c0e4c4dbb99f262767c
Priyanshu pandey <priyanshupandeyuse111@gmail.com>
2025-08-28 23:29:58 +0530

    finaly deploying

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 dotenv.config();
 const cors = require("cors");
-const serverless = require('serverless-http');
+
 const app = express();
 
 // Middlewares
@@ -31,13 +31,8 @@ app.use("/api/user", user);
 app.get("/", (req, res) => res.json({ message: "API running!" }));
 
 // For local development
-// if (process.env.NODE_ENV !== "production") {
-//     const PORT = process.env.PORT || 5000;
-//     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-// }
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // Export app for Vercel
-app.get("/api/test", (req, res) => {
-    res.json({ message: "Test endpoint working!" });
-});
-module.exports = serverless(app);
+
+    
